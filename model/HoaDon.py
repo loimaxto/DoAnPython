1e9f2cd4f751b0f2c50e2ae7a063a83b5e4ad1fa
Vo Dinh Kiet <vokiet2534@gmail.com>
2025-02-26 19:17:18 +0700

    creat branches

---
+++ model/HoaDon.py
@@ -0,0 +1,3 @@
+class HoaDon:
+    # thuộc tính
+    
---
+++ model/NhanVien.py
@@ -0,0 +1,12 @@
+class NhanVien:
+    # thuộc tính
+    id = str()
+    name = str()
+    taiKhoan = str()
+    matKhau = str()
+
+    # phương thức
+    def showInfo(self):
+        pass
+    def nhapInfo(self):
+        pass
