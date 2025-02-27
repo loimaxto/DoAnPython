314237e7b2267601d0b233b1c4529ac102f06d98
Vo Dinh Kiet <vokiet2534@gmail.com>
2025-02-27 10:45:39 +0700

    Thêm giao diện đặt phòng và quản lý hóa đơn

--- model/HoaDon.py
+++ model/HoaDon.py
@@ -1,3 +1,13 @@
+from datetime import date
 class HoaDon:
     # thuộc tính
+    id = str()
+    id_phong = str()
+    id_khach = str()
+    id_menu = str()
+    SoluongItems = int()
+    NgayThue = date()
+    NgayTra = date()
+    ThanhTien = int()
+
     
