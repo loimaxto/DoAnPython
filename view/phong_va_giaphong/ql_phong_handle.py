8020279bdaf4f135e276781fe5bc98ef46f10218
Vo Dinh Kiet <vokiet2534@gmail.com>
2025-03-05 21:33:54 +0700

    xong chức năng ql phòng, có thông báo khi input  lỗi

--- view/phong_va_giaphong/ql_phong_handle.py
+++ view/phong_va_giaphong/ql_phong_handle.py
@@ -1,9 +1,9 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QTableWidgetItem, QMessageBox
-from ql_gia_phong import Ui_Form
+from PyQt6.QtWidgets import QWidget, QMessageBox, QTableWidgetItem
 import sqlite3
+from ql_phong import Ui_Form
 
-class gia_phong(Ui_Form):
+class ql_phong(Ui_Form):
     def __init__(self, Form):
         self.setupUi(Form)
 
@@ -18,9 +18,9 @@ class gia_phong(Ui_Form):
         self.del_btn.clicked.connect(self.delete_item)
         self.edi_btn.clicked.connect(self.update_item)
         self.sea_btn.clicked.connect(self.search_item)
-    
+
     def show_all(self):
-        self.cursor.execute("select * from gia_phong")
+        self.cursor.execute("select * from phong")
         data = self.cursor.fetchall()
         self.dis_pla.setRowCount(0)
         for row_index, row_data in enumerate(data):
@@ -33,13 +33,12 @@ class gia_phong(Ui_Form):
     def insert_item(self):
         # dữ liệu đầu vào
         id = self.in_id.text()
-        name = self.in_name.text()
-        gio = self.in_hour.text()
-        ngay = self.in_day.text()
-        dem = self.in_night.text()
+        name = self.in_ten.text()
+        sogiuong = self.in_sg.text()
+        id_gia = self.in_price_id.text()
 
         try:
-            self.cursor.execute("insert into gia_phong values(?, ?, ?, ?, ?)", (id, name, gio, ngay, dem))
+            self.cursor.execute("insert into phong values(?, ?, ?, ?)", (id, name, sogiuong, id_gia))
             self.conn.commit()
             print("Da them")
         except:
@@ -54,7 +53,7 @@ class gia_phong(Ui_Form):
         id = self.in_id.text()
 
         try:
-            self.cursor.execute("delete from gia_phong where gia_id=?", (id,))
+            self.cursor.execute("delete from phong where id=?", (id,))
             self.conn.commit()
             print("da xoa")
         except:
@@ -67,16 +66,15 @@ class gia_phong(Ui_Form):
         self.show_all()
     def update_item(self):
         id = self.in_id.text()
-        name = self.in_name.text()
-        gio = self.in_hour.text()
-        ngay = self.in_day.text()
-        dem = self.in_night.text()
+        name = self.in_ten.text()
+        sogiuong = self.in_sg.text()
+        id_gia = self.in_price_id.text()
 
         try:
-            self.cursor.execute("update gia_phong\
-                                set ten_loai=?, gia_gio=?, gia_ngay=?, gia_dem=?\
-                                where gia_id=?\
-                                ", (name, gio, ngay, dem, id))
+            self.cursor.execute("update phong\
+                                set ten_phong=?, so_giuong=?, id_gia=?\
+                                where id=?\
+                                ", (name, sogiuong, id_gia, id))
             self.conn.commit()
             print("ok")
         except:
@@ -89,7 +87,7 @@ class gia_phong(Ui_Form):
         self.show_all()
     def search_item(self):
         id = self.in_sea.text()
-        self.cursor.execute("select * from gia_phong where gia_id=?", (id,))
+        self.cursor.execute("select * from phong where id=?", (id,))
         data = self.cursor.fetchall()
         self.dis_pla.setRowCount(0)
         for row_index, row_data in enumerate(data):
@@ -97,12 +95,10 @@ class gia_phong(Ui_Form):
             for column_index, item_data in enumerate(row_data):
                 self.dis_pla.setItem(row_index, column_index, QTableWidgetItem(str(item_data)))
 
-
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = gia_phong(Form)
+    ui = ql_phong(Form)
     Form.show()
     sys.exit(app.exec())
