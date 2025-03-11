44376dfd8f5aa7380c135f54a170b23f710bf120
quang <121563086+loimaxto@users.noreply.github.com>
2025-03-11 07:32:48 +0700

    connect myMain with datphong_page and customer_page

--- myMain.py
+++ myMain.py
@@ -5,6 +5,7 @@ from view.Menu.page1 import Page1
 from view.Menu.page2 import Page2
 
 from view.khach_hang.kh_handle import CustomerManagementWindow
+from view.DatPhong.handle_dat_phong import DatPhongWindow
 from view.Menu.menu_ui import Ui_MainWindow
 class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
@@ -14,10 +15,15 @@ class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
         self.ui.setupUi(self)
         
         self.kh_page = CustomerManagementWindow()
+        self.dp_page = DatPhongWindow()
+        
+        self.ui.stackedWidget.addWidget(self.dp_page)
         self.ui.stackedWidget.addWidget(self.kh_page)
+        
         self.ui.stackedWidget.setCurrentWidget(self.kh_page)
+        
         self.ui.khachHangBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.kh_page))
-        self.ui.kh_page = CustomerManagementWindow()
+        self.ui.datPhongBtn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.dp_page))
 
 
         
