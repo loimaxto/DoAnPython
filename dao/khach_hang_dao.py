d5c8080918a94af7d9eac7cd74c397308f000220
quang <121563086+loimaxto@users.noreply.github.com>
2025-03-12 18:00:12 +0700

    almost finish KH page, still have update left

--- dao/khach_hang_dao.py
+++ dao/khach_hang_dao.py
@@ -29,7 +29,11 @@ class KhachHangDAO:
             return KhachHangDTO(kh_id=row[0], ten=row[1], sdt=row[2], image=row[3])
         else:
             return None
-
+    def get_khach_hang_next_id(self):
+        query = "SELECT COALESCE(MAX(kh_id), 0) + 1 FROM khach_hang;"
+        rs = self.db.execute_query(query)
+        id = rs[0][0]
+        return id
     def insert_khach_hang(self, khach_hang):
         query = "INSERT INTO khach_hang (ten, sdt, image) VALUES (?, ?, ?)"
         insert_row_data = self.db.execute_query(query, (khach_hang.ten, khach_hang.sdt, khach_hang.image), return_last_row=True)
@@ -55,6 +59,9 @@ class KhachHangDAO:
 # Example usage (in a separate main.py or similar):
 if __name__ == "__main__":
     dao = KhachHangDAO()
+    print('ass')
+    a= dao.get_khach_hang_next_id()
+    print(a)
 """
     # Example insert
     new_customer = KhachHangDTO(ten="Doe a", sdt="123-456-7890", image="john.jpg")
